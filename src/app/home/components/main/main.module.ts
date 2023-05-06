bab2c64c3197afbb5876be98c6e96c069fe304b0
Ilkin Rufullayev <rufullayevilkin66@gmail.com>
2023-05-06 14:11:16 +0400

    main page is ready

--- src/app/home/components/main/main.module.ts
+++ src/app/home/components/main/main.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { PostModule } from '../post/post.module';
 
 
 
 @NgModule({
-  declarations: [
-    MainComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    MainComponent
-  ]
+    declarations: [
+        MainComponent
+    ],
+    exports: [
+        MainComponent
+    ],
+    imports: [
+        CommonModule,PostModule
+    ]
 })
 export class MainModule { }
